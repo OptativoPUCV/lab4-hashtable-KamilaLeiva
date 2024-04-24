aa22435a2b10c719bd91458bfad613c2a23849b4
KamilaLeiva <163158363+KamilaLeiva@users.noreply.github.com>
2024-04-24 20:51:45 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -96,8 +96,14 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  int i;
+  for(i=0;i<map->capacity;i++){
+    if(map->buckets[i]!=NULL){
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
