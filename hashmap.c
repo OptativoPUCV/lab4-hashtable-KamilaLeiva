43df1ef8d36a1930c4c4c99c3a1260fc5ec4e7f0
KamilaLeiva <163158363+KamilaLeiva@users.noreply.github.com>
2024-04-24 21:15:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -97,12 +97,12 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
   int i;
-  for(i=0;i<map->capacity;i++)
+  for(i=0;i<map->capacity;i++){
     if(map->buckets[i]!=NULL){
       map->current = i;
       return map->buckets[i];
     }
-  
+  }
   return NULL;
 }
 
