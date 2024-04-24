fdca3ebb1694f49f8b1769c7a2c8c8a913dfdd55
KamilaLeiva <163158363+KamilaLeiva@users.noreply.github.com>
2024-04-24 21:06:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -91,12 +91,11 @@ void eraseMap(HashMap * map,  char * key) {
       }
       index = (index + 1) % map->size;
   }
-  }
-  
-  
-
-
 }
+  
+  
+
+
 
 Pair * searchMap(HashMap * map,  char * key) {   
   int posp = hash(key,map->capacity);
