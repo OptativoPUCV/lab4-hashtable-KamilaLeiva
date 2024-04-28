d52d6924594f19990122f2c5594b627d0e6da13f
KamilaLeiva <163158363+KamilaLeiva@users.noreply.github.com>
2024-04-28 02:51:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -134,7 +134,7 @@ Pair * nextMap(HashMap * map) {
 
   int i;
   for(i=map->current+1;i<map->capacity;i++){
-    if(map->buckets[i]!=NULL){
+    if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
       map->current = i;
       return map->buckets[i];
     }
